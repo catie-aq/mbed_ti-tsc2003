28943acdec5e868d9c073f01b6a967a72d7bfcaa
Clément Laigle <c.laigle@catie.fr>
2023-07-25 09:51:24 +0200

    Add write command function

--- src/tsc2003ipwr.cpp
+++ src/tsc2003ipwr.cpp
@@ -6,8 +6,20 @@
 
 namespace sixtron {
 
-TSC2003IPWR::TSC2003IPWR()
+TSC2003IPWR::TSC2003IPWR(PinName sda, PinName scl, PinName irq, i2c_address i2c_address):
+        _i2c(sda, scl), _i2c_address(i2c_address)
 {
+}
+
+int TSC2003IPWR::i2c_write_command(function func, power_down power, mode mode)
+{
+    uint8_t cmd = (uint8_t)func << 4;
+    cmd |= (uint8_t)power << 2;
+    cmd |= (uint8_t)mode << 1;
+
+    if (_i2c.write(_i2c_address, (char *)&mode, 1, true) != 0) {
+        return -1;
+    }
 }
 
 } // namespace sixtron
--- tsc2003ipwr/tsc2003ipwr.h
+++ tsc2003ipwr/tsc2003ipwr.h
@@ -5,11 +5,54 @@
 #ifndef CATIE_SIXTRON_TSC2003IPWR_H_
 #define CATIE_SIXTRON_TSC2003IPWR_H_
 
+#include "mbed.h"
+
 namespace sixtron {
 
 class TSC2003IPWR {
 public:
-    TSC2003IPWR();
+    typedef enum {
+        ADDRESS1 = (0x48), // A0 and A1 connected to GND
+        ADDRESS2 = (0x49), // A0 connected to VDD and A1 connected to GND
+        ADDRESS3 = (0x4A), // A0 connected to GND and A1 connected to VCC
+        ADDRESS4 = (0x4B), // A0 and A1 connected to VDD
+    } i2c_address;
+
+    typedef enum {
+        MEASURE_TEMP0 = 0,
+        MEASURE_BAT1 = 1,
+        MEASURE_IN1 = 2,
+        MEASURE_TEMP1 = 4,
+        MEASURE_BAT2 = 5,
+        MEASURE_IN2 = 6,
+        ACTIVATE_X = 8,
+        ACTIVATE_Y = 9,
+        ACTIVATE_YPLUS_X = 10,
+        MEASURE_X = 12,
+        MEASURE_Y = 13,
+        MEASURE_Z1 = 14,
+        MEASURE_Z2 = 15
+    } function;
+
+    typedef enum {
+        POWERDOWN_IRQON = 0,
+        INTREFOFF_ADCON_IRQOFF = 1,
+        INTREFON_ADCOFF_IRQON = 1,
+        INTREFON_ADCON_IRQOFF = 3
+    } power_down;
+
+    typedef enum {
+        _12BITS = 0,
+        _8BITS = 1
+    } mode;
+
+    TSC2003IPWR(PinName sda, PinName scl, PinName irq, i2c_address i2c_address);
+
+private:
+    I2C _i2c;
+    i2c_address _i2c_address;
+
+    int i2c_write_command(function func, power_down power, mode mode);
 };
 
 } // namespace sixtron
